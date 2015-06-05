cf7c675a31f0f7cb200e37d2dc54e361f1a86986
Oleksandr Bogovyk <obogovyk@gmail.com>
2015-06-05 10:25:02 +0300

    Rewrite argparse. Add 32 characters.

--- pgutil.py
+++ pgutil.py
@@ -1,70 +1,64 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# pgutil v.1.0.14
 
 '''
+PGUtil v0.1.21
+Simple, but strong password generator utility.
+
+This program comes with ABSOLUTELY NO WARRANTY; for details type `pgutil --help'.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+
 Copyright(C) 2015 Bogovyk Oleksandr <obogovyk@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import random, string
 import argparse
+import sys
 import os
 
-parser = argparse.ArgumentParser(description='Help for pgutil.')
-parser.add_argument('-c', '--count', type=int, default=6, help="Password count number.")
-parser.add_argument('-l', '--lenght', type=int, default=8, help="Password lenght number.")
-parser.add_argument('-n', '--numbers', type=str, default=False, help="Use numbers.")
-parser.add_argument('-s', '--symbols', type=str, default=False, help="Use special symbols like '!@#$&*()'.")
+parser = argparse.ArgumentParser(description='Help for PGUtil v0.1.21')
+parser.add_argument('-c', '--count', type=int, default=5, choices=range(1,100), help="Password count number (5 is default)")
+parser.add_argument('-l', '--lenght', type=int, default=8, choices=range(6,16), help="Password lenght number (8 is default)")
+parser.add_argument('-n', '--numbers', action='store_true', default=False, help="Use numbers in password.")
+parser.add_argument('-s', '--symbols', action='store_true', default=False, help="Use special symbols in password like !()@#$&.,?{}*.")
 
 class pgutil:
 
-    numbers = ""
-    symbols = ""
-
     pg_string = ""
+    pg_list = []
 
-    def __init__(self, count, lenght, is_numbers, is_symbols, is_unique):
+    def __init__(self, count, lenght, is_numbers, is_symbols):
         self.count = count
         self.lenght = lenght
         self.is_numbers = is_numbers
         self.is_symbols = is_symbols
-        self.is_unique = is_unique
 
     def pg_generate(self):
 
-        if(self.is_numbers == "True"):
+        if(self.is_numbers is True):
             self.numbers = string.digits
         else:
             self.numbers = str("")
 
-        if(self.is_symbols == "True"):
-            self.symbols = "!@#$%^&*()"
+        if(self.is_symbols is True):
+            self.symbols = "`~!@#$%^&*()-_=+[]{};:,<.>?"
         else:
             self.symbols = str("")
 
-        self.pg_string = self.numbers + string.ascii_letters + self.symbols
+        self.pg_string = string.ascii_letters + self.numbers + self.symbols
 
         for x in range(0,self.count):
-            random.seed = (os.urandom(2048))
-            print ''.join(random.choice(self.pg_string) for i in range(self.lenght))
+            random.seed = (os.urandom(32))
+            self.pg_list.append(''.join(random.choice(self.pg_string) for i in range(self.lenght)))
+            
+        for password in self.pg_list:
+            print(password)
+        
+if __name__ == "__main__":
+    pg = pgutil(parser.parse_args().count,
+    parser.parse_args().lenght,
+    parser.parse_args().numbers,
+    parser.parse_args().symbols)
 
-pg = pgutil(parser.parse_args().count,
-            parser.parse_args().lenght,
-            parser.parse_args().numbers,
-            parser.parse_args().symbols,
-            parser.parse_args().unique)
-
-pg.pg_generate()
+    pg.pg_generate()
